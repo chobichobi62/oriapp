792b00b962481f7cbc74e4894e743cce2528ba4c
chobichobi62 <ono.t@ymail.ne.jp>
2024-05-01 18:28:10 +0900

    Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'homes/index'
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
