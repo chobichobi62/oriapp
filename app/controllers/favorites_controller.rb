25584645d006451c107de967beabba75d90f91e8
chobichobi62 <ono.t@ymail.ne.jp>
2024-05-09 12:28:50 +0900

    非同期処理・ハートマークの導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :set_recipe
+
   def create
     favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
     favorite.save
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
