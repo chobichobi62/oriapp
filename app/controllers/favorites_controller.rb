c5d2eb36dbce1d633ade3b61277bac1031dab344
chobichobi62 <ono.t@ymail.ne.jp>
2024-05-11 16:14:45 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,18 +3,18 @@ class FavoritesController < ApplicationController
   before_action :set_variables
 
   def create
-    favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
-    favorite.save
+    add_favorite
     respond_to do |format|
+      format.turbo_stream { redirect_to @recipe }
       format.js
     end
   end
 
   def destroy
-    favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
-    favorite.destroy
-    respond_to do |format|
-      format.js
+    remove_favorite
+  respond_to do |format|
+    format.turbo_stream { redirect_to @recipe }
+    format.js
     end
   end
 
@@ -22,19 +22,19 @@ class FavoritesController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
   end
 
-  def favorite
-    favorite = current_user.favorites.new(recipe_id: @recipe.id)
+  private
+
+  def add_favorite
+    favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
     favorite.save
   end
 
-  def favorite
-    favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
-    favorite.destroy
-  end
+  def remove_favorite
+  favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
+  favorite.destroy
+end
 
-  private
   def set_variables
-    @recipe = Recipe.find(params[:recipe_id])
     @id_name = "#like-link-#{@recipe.id}"
   end
 end
