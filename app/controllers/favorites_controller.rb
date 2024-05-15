e65bfff6d675fb39b7f5d3f0d96afbf1c4aa5f1d
chobichobi62 <ono.t@ymail.ne.jp>
2024-05-15 10:53:28 +0900

    いいね機能　修正6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,18 @@
 class FavoritesController < ApplicationController
   before_action :set_recipe
+  before_action :authenticate_user!
+
 
   def create
+    @recipe = Recipe.find(params[:recipe_id])
+    current_user.favorite_recipes << @recipe
+    redirect_to @recipe, notice: 'お気に入りに登録しました。'
   end
 
   def destroy
-
+    @recipe = Recipe.find(params[:recipe_id])
+    current_user.favorite_recipes.delete(@recipe)
+    redirect_to @recipe, notice: 'お気に入りを解除しました。'
 end
 
 
