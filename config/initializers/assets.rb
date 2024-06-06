ecf084cb0892dfa651d9879a1d68db5510945f17
chobichobi62 <ono.t@ymail.ne.jp>
2024-06-06 16:01:17 +0900

    Gem修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,9 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+
+require 'autoprefixer-rails'
+
+Rails.application.config.assets.configure do |env|
+  env.register_postprocessor 'text/css', AutoprefixerRails.processor
+end
